e0327aee5bf4df6401f5613b676b1255c479c07a
Ruben <ruben.infantes@gmail.com>
2021-05-13 20:25:55 +0200

    Proyecto iniciado

---
+++ Icarvs-back/src/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const password = "icarvsPass"
+const dbname = "Users"
+const user = "Icarvs"
+const host = "cluster0.grpbn.mongodb.net"
+
+const uri = `mongodb+srv://${user}:${password}@${host}/${dbname}?retryWrites=true&w=majority`
+
+module.exports = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+
